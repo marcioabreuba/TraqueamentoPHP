cf955f2c4f3f264e4cff743d5a2651bbb8bd868b
Marcio Abreu <marcioabreu.ba@gmail.com>
2025-05-10 14:02:00 -0300

    Commit inicial limpo do projeto de rastreamento

---
+++ config/conversions.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'domains' => [
+        'salve-terrah-shopify' => [
+            'pixel_id' => '1163339595278098',
+            'access_token' => 'EAAHZAkMjZBhOkBOyNMyPlwzhsX3ruT2cUAQCe8pqOFyuD15BzCspoQ8VZAMWahuI5yTmHUsIDIURs7uSOXH0DOZBKXsu9sFlhaYTC7sQ5o5dVYIYggiZBFDLZAEyZC63qPjvhe4Q8RbxX7yMCWDaULYJ2TA8fKFJDPMweuR44ayRdDVWziDm1LYrLOgZC3LIBWbKPgZDZD',
+            'test_code' => '',
+        ],
+    ]
+];
